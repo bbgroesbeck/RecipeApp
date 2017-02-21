4da19f0c7ae4c1d569a83e0b195b02d8a47f497f
brycegroesbeck <bbgroesbeck@xactware.com>
2017-02-21 07:51:55 -0700

    added auth

--- recipe/src/app/app.module.ts
+++ recipe/src/app/app.module.ts
@@ -4,6 +4,17 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { AngularFireModule } from 'angularfire2';
+
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyBVR8A-Mawq6aJiudG-i1Aes_dVf3JTovw",
+  authDomain: "recip-311bc.firebaseapp.com",
+  databaseURL: "https://recip-311bc.firebaseio.com",
+  storageBucket: "recip-311bc.appspot.com",
+  messagingSenderId: "779087682601"
+};
+
 
 @NgModule({
   declarations: [
@@ -18,3 +29,4 @@ import { AppComponent } from './app.component';
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
